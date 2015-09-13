786203be0993bca4e16ae6d30e687cad41240f83
Egor Machnev <egork1996@yandex.ru>
2015-09-13 03:04:02 +0300

    Create fragment

---
+++ app/src/main/java/com/example/egor/devcolibri_remindme/Fragment.java
@@ -0,0 +1,19 @@
+package com.example.egor.devcolibri_remindme;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by egor on 13.09.15.
+ */
+public class Fragment extends android.app.Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
